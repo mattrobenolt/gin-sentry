7deb4c29b7b05f0095c075fc773a2a7761980a35
Matt Robenolt <matt@ydekproductions.com>
2015-04-09 11:16:21 -0400

    Capture an actual Request interface

--- recovery.go
+++ recovery.go
@@ -12,20 +12,17 @@ import (
 func Recovery(client *raven.Client, onlyCrashes bool) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
-			flags := map[string]string{
-				"endpoint": c.Request.RequestURI,
-			}
 			if rval := recover(); rval != nil {
 				debug.PrintStack()
 				rvalStr := fmt.Sprint(rval)
-				packet := raven.NewPacket(rvalStr, raven.NewException(errors.New(rvalStr), raven.NewStacktrace(2, 3, nil)))
-				client.Capture(packet, flags)
+				packet := raven.NewPacket(rvalStr, raven.NewException(errors.New(rvalStr), raven.NewStacktrace(2, 3, nil)), raven.NewHttp(c.Request))
+				client.Capture(packet, nil)
 				c.Writer.WriteHeader(http.StatusInternalServerError)
 			}
 			if !onlyCrashes {
 				for _, item := range c.Errors {
 					packet := raven.NewPacket(item.Err, &raven.Message{item.Err, []interface{}{item.Meta}})
-					client.Capture(packet, flags)
+					client.Capture(packet, nil)
 				}
 			}
 		}()
